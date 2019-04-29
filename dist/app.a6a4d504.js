4d40c50ad6b8b661f7eee9b5d3a598496cd596a3
dashixiong <2788347834@qq.com>
2019-04-30 01:03:17 +0800

    添加 iconposition验证

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12422,8 +12422,18 @@ exports.default = void 0;
 //
 //
 //
-//
-var _default = {};
+var _default = {
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator: function validator(value) {
+        return value == 'left' || value == 'right';
+      }
+    }
+  }
+};
 exports.default = _default;
         var $603f7f = exports.default || module.exports;
       
@@ -12434,23 +12444,26 @@ exports.default = _default;
         /* template */
         Object.assign($603f7f, (function () {
           var render = function() {
+  var _obj
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", [
-    _c(
-      "button",
-      { staticClass: "g-button" },
-      [
-        _c("svg", { staticClass: "icon", attrs: { "aria-hidden": "true" } }, [
-          _c("use", { attrs: { "xlink:href": "#i-setting" } })
-        ]),
-        _vm._v(" "),
-        _vm._t("default")
-      ],
-      2
-    )
-  ])
+  return _c(
+    "button",
+    {
+      staticClass: "g-button",
+      class: ((_obj = {}), (_obj["icon-" + _vm.iconPosition] = true), _obj)
+    },
+    [
+      _vm.icon
+        ? _c("svg", { staticClass: "icon", attrs: { "aria-hidden": "true" } }, [
+            _c("use", { attrs: { "xlink:href": "#i-" + _vm.icon } })
+          ])
+        : _vm._e(),
+      _vm._v(" "),
+      _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
+    ]
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
--- src/button.vue
+++ src/button.vue
@@ -1,17 +1,26 @@
 <template>
-  <div>
-    <button class='g-button'>
-<svg class="icon" aria-hidden="true">
-    <use xlink:href="#i-setting"></use>
-</svg>      
-      <slot></slot>
+  <button class='g-button' :class='{[`icon-${iconPosition}`]:true}'>
+    <svg v-if="icon" class="icon" aria-hidden="true">
+      <use :xlink:href=`#i-${icon}`></use>
+    </svg>      
+      <div class="content">
+        <slot></slot>
+      </div>
     </button>
-  </div>
-
 </template>
 
 <script>
   export default {
+    props:{
+      icon:{},
+      iconPosition:{
+        type:String,
+        default:'left',
+        validator(value){
+          return (value == 'left' || value == 'right')
+        }
+      }
+    }
 
   }
 
@@ -19,6 +28,9 @@
 
 <style lang='scss'>
 .g-button{
+  display:inline-flex;
+  justify-content:center;
+  align-items:center;
   height:var(--button-height);
   padding:0 1em;
   font:inherit;
@@ -26,15 +38,33 @@
   border:1px solid var(--border-color);
   font-size:var(--font-size);
   height:var(--button-height);
-}
-.g-button:hover{
-  border-color:var(--border-color-hover);
-}
-.g-button:active{
-  background-color:var(--button-active-bg);
-}
-.g-button:focus{
-  outline:none;
-}
+  vertical-align:middle;
+    > .icon{
+      order:1;
+      margin-right:.1em;
+    }
+    > .content{
+      order:2;
+    }
+  &:hover{
+    border-color:var(--border-color-hover);
+  }
+  &:active{
+    background-color:var(--button-active-bg);
+  }
+  &:focus{
+    outline:none;
+  }
+  &.icon-right{
+    > .icon{
+      order:2;
+      margin-left:.1em;
+      margin-right:0;
+    }
+    > .content{
+      order:1;
+    }
+  }
+        }
 
 </style>
