aded82f4c002c2f537f8a864ba21da7210d83dfb
dashixiong <2788347834@qq.com>
2019-04-29 21:19:53 +0800

    完成按钮基本样式

---
+++ src/button.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <button class='g-button'>
+<svg class="icon" aria-hidden="true">
+    <use xlink:href="#i-setting"></use>
+</svg>      
+      <slot></slot>
+    </button>
+  </div>
+
+</template>
+
+<script>
+  export default {
+
+  }
+
+</script>
+
+<style lang='scss'>
+.g-button{
+  height:var(--button-height);
+  padding:0 1em;
+  font:inherit;
+  border-radius:var(--border-radius);
+  border:1px solid var(--border-color);
+  font-size:var(--font-size);
+  height:var(--button-height);
+}
+.g-button:hover{
+  border-color:var(--border-color-hover);
+}
+.g-button:active{
+  background-color:var(--button-active-bg);
+}
+.g-button:focus{
+  outline:none;
+}
+
+</style>
