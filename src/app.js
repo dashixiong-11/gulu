cafcd1e8f590b141b8cfb1c9775abb1c685fa117
dashixiong <2788347834@qq.com>
2019-04-30 22:20:32 +0800

    使用chai.exoect 添加四个测试用例，第五个不会

--- src/app.js
+++ src/app.js
@@ -15,3 +15,75 @@ new Vue({
   }
 })
 
+import chai from 'chai'
+const expect = chai.expect
+{
+  const Contructor = Vue.extend(Button)
+  const button = new Contructor({
+    propsData:{
+      icon:'setting'
+    }
+  }).$mount()
+  let useElement = button.$el.querySelector('use')
+  expect(useElement.getAttribute('xlink:href')).to.eq('#i-setting')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const Contructor = Vue.extend(Button)
+  const button = new Contructor({
+    propsData:{
+      icon:'setting',
+      loading:true
+    }
+  }).$mount()
+  let useElement = button.$el.querySelector('use')
+  let herf = useElement.getAttribute('xlink:href')
+  expect(herf).to.eq('#i-jiazaizhong')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const Contructor = Vue.extend(Button)
+  const button = new Contructor({
+    propsData:{
+      icon:'setting',
+      loading:true
+    }
+  }).$mount('#test')
+  let svg = button.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const  div = document.createElement('div')
+  document.body.appendChild(div)
+  const Contructor = Vue.extend(Button)
+  const button = new Contructor({
+    propsData:{
+      icon:'setting',
+      loading:true,
+      iconPosition:'right'
+    }
+  }).$mount(div)
+  let svg = button.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const Contructor = Vue.extend(Button)
+  const gbutton = new Contructor({
+    propsData:{
+      icon:'setting',
+    }
+  }).$mount()
+  gbutton.$on('click',function(){
+    console.log(1)
+  })
+  let button = gbutton.$el
+  button.$el.click()
+}
