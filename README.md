29b0bc469a7b09d1884c3032976575ae43b6dd57
dashixiong <2788347834@qq.com>
2019-05-19 19:20:30 +0800

    把icon 移到本地

--- README.md
+++ README.md
@@ -13,11 +13,23 @@
 
 使用本框架前，请先设置css:
 ```
-
-*{
-    box-sizing:border-box;
-}
+*,*::befoer,*::after{ box-sizing:border-box; }
 ```
+你还需要设置默认颜色等变量
+```
+html{
+  --button-height:32px;
+  --button-bg:white;
+  --font-size:14px;
+  --button-active-bg:#eee;
+  --border-radius:4px;
+  --color:#333;
+  --border-color:#999;
+  --border-color-hover:#666;
+  }
+```
+
+IE 8 及以上支持。
 
 ## 文档
 
--- src/icon.vue
+++ src/icon.vue
@@ -1,17 +1,18 @@
 <template>
-    <svg  class="icon" aria-hidden="true">
+    <svg  class="g-icon" aria-hidden="true">
       <use :xlink:href=`#i-${name}`></use>
     </svg>      
 </template>
 
 <script>
+  import './svg'
   export default{
     props:['name']
   }
 
 </script>
 <style lang='scss'>
-    .icon {
+    .g-icon {
        width: 1em; height: 1em;
        vertical-align: -0.15em;
        fill: currentColor;
