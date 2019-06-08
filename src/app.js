2fd86059d37e8d08b9e0641ef2a3a77e99cfb9c9
dashixiong <2788347834@qq.com>
2019-06-08 19:52:41 +0800

    布局组件 基本完成

--- src/app.js
+++ src/app.js
@@ -3,17 +3,26 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
-import Picker from './address'
 import Row from './row'
 import Col from './col'
+import Layout from  './layout'
+import Header from './header'
+import Sider from './sider'
+import Content from './content'
+import Footer from './footer'
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
 Vue.component('g-button-group',ButtonGroup)
 Vue.component('g-input',Input)
-Vue.component('g-picker',Picker)
 Vue.component('g-row',Row)
 Vue.component('g-col',Col)
+Vue.component('g-layout',Layout)
+Vue.component('g-header',Header)
+Vue.component('g-sider',Sider)
+Vue.component('g-content',Content)
+Vue.component('g-footer',Footer)
+
 
 new Vue({
   el:'#app',
