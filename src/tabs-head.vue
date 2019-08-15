d2c3c21b3918c13370b46a299f506399d9ab8ea2
dashixiong <2788347834@qq.com>
2019-08-15 11:16:32 +0800

    添加tabs相关组件

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,17 +1,30 @@
 <template>
-    <div class="tabs-header">
+    <div class="tabs-head">
         <slot></slot>
-        <slot name="actions"></slot>
+        <div class="actions-wrapper">
+            <slot name="actions"></slot>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name:'GuluTabsHeader'
+        name:'GuluTabsHeader',
+        inject:['eventBus']
     }
 
 </script>
 
-<style>
-
+<style scoped lang="scss">
+    $tab-height: 40px;
+    .tabs-head{
+        display: flex;
+        height: $tab-height;
+        justify-content: flex-start;
+        align-items: center;
+        border:1px solid red;
+        >.actions-wrapper{
+            margin-left:auto;
+        }
+    }
 </style>
--- src/tabs.vue
+++ src/tabs.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+    import Vue from 'vue'
     export default {
         name:'GuluTabs',
         props:{
@@ -21,8 +22,18 @@
                 }
             }
         },
-        created() {
-/*            this.$emit('update:selected','')*/
+        data(){
+          return{
+              eventBus:new Vue()
+          }
+        },
+        provide(){
+            return {
+                eventBus:this.eventBus
+            }
+        },
+        mounted() {
+            this.eventBus.$emit('update:selected',this.selected)
         }
     }
 
