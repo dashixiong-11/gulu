3766dfa1f6248178ef350be477de1948a72152c7
dashixiong <2788347834@qq.com>
2019-11-12 17:47:09 +0800

    支持手机滑动 ，上下页

--- src/slides.vue
+++ src/slides.vue
@@ -1,22 +1,32 @@
 <template>
-   <div class="g-slides" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+   <div class="g-slides" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave"
+        @touchstart="onTouchStart"
+        @touchmove="onTouchMove"
+        @touchend="onTouchEnd"
+   >
       <div class="g-slides-window" ref="window">
          <div class="g-slides-wrapper">
             <slot></slot>
          </div>
       </div>
       <div class="g-slides-dots">
-         <span v-for="n in childrenLength" :key="n" :class="{active: selectedIndex === n-1}"
-               @click="select(n-1)"
-         >{{n-1}}</span>
+         <span @click="select(selectedIndex - 1)">
+            <g-icon name="left"></g-icon>
+         </span>
+         <span v-for="n in childrenLength" :key="n" :class="{active: selectedIndex === n-1}" @click="select(n-1)" >{{n}}</span>
+         <span @click="select(selectedIndex + 1)">
+            <g-icon name="right"></g-icon>
+         </span>
       </div>
    </div>
 </template>
 
 <script>
+   import gIcon from './icon'
    /* $children 只显示子组件， 获取元素的children 可以用 $el */
    export default {
       name: "slides",
+      components:{ gIcon },
       props: {
          selected: {
             type: String,
@@ -30,21 +40,26 @@
          return{
             childrenLength:0,
             lastSelectedIndex: undefined,
-            timerId: undefined
+            timerId: undefined,
+            startTouch:undefined
          }
 
       },
       mounted() {
          this.updateChildren()
          this.playAutomatically()
-         this.childrenLength = this.$children.length
+         this.childrenLength = this.items.length
       },
       computed:{
          selectedIndex(){
-            return this.names.indexOf(this.selected) || 0
+            let index = this.names.indexOf(this.selected)
+            return index === -1? 0 : index
          },
          names(){
-            return this.$children.map( vm => vm.name)
+            return this.items.map( vm => vm.name)
+         },
+         items(){
+            return  this.$children.filter( vm =>  vm.$options.name === 'GuluSlidesItem')
          }
       },
       methods: {
@@ -54,9 +69,35 @@
          onMouseLeave(){
             this.playAutomatically()
          },
-         select(index){
+         onTouchStart(e){
+            this.pause()
+            this.startTouch = e.touches[0]
+         },
+         onTouchMove(){
+         },
+         onTouchEnd(e){
+            let endTouch = e.changedTouches[0]
+            let {clientX:x1,clientY:y1} = this.startTouch
+            let {clientX:x2,clientY:y2} = endTouch
+            let distance = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2))
+            let deltaY = Math.abs(y2 - y1)
+            let rate = distance / deltaY
+            if(rate>2){
+               if(x2 > x1){
+                  this.select(this.selectedIndex - 1)
+               }else{
+                  this.select(this.selectedIndex + 1)
+               }
+            }
+            this.$nextTick( ()=>{
+               this.playAutomatically()
+            })
+         },
+         select(newIndex){
+            if(newIndex === -1) { newIndex = this.names.length - 1 }
+            if(newIndex === this.names.length){ newIndex = 0}
             this.lastSelectedIndex = this.selectedIndex
-            this.$emit('update:selected',this.names[index])
+            this.$emit('update:selected',this.names[newIndex])
          },
          playAutomatically(){
             if(this.timerId) { return } //如果当前有动画 就不添加定时器 防止刷新的时候鼠标就已经在轮播上了
@@ -64,8 +105,6 @@
                let index = this.names.indexOf(this.getSelected())
                if(index === this.names.length){ index = 0 }
                let newIndex = index + 1
-               if(newIndex === -1) { newIndex = this.names.length - 1 }
-               if(newIndex === this.names.length){ newIndex = 0}
                this.select(newIndex)
                this.timerId = setTimeout(run,3000)
             }
@@ -76,16 +115,19 @@
             this.timerId = undefined
          },
          getSelected(){
-            let first = this.$children[0]
+            let first = this.items[0]
             return  this.selected || first.name
          },
          updateChildren() {
             let selected = this.getSelected()
-            this.$children.forEach(vm => {
+            this.items.forEach(vm => {
                let reverse = this.selectedIndex > this.lastSelectedIndex ? false : true  //在这边修改子组件的reverse 有可能并不会马上生效
-               if(this.lastSelectedIndex  === this.names.length -1 && this.selectedIndex === 0){
-                  reverse = false
-               }
+                  if(this.lastSelectedIndex  === this.names.length -1 && this.selectedIndex === 0){
+                     reverse = false
+                  }
+                  if(this.lastSelectedIndex === 0 && this.selectedIndex === this.names.length -1){
+                     reverse = true
+                  }
                vm.reverse = reverse
                //就会造成reverse 还留在class里的情况
                this.$nextTick(()=>{ //解决reverse 更新不及时的bug
@@ -109,10 +151,28 @@
          position: relative;
       }
       .g-slides-dots{
+         padding: 4px 0;
+         display: flex;
+         align-items: center;
+         justify-content: center;
          >span{
+            width: 1.2em;
+            height: 1.2em;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background: #ddd;
+            border-radius: 50%;
+            margin: 0 .2em;
+            &:hover{
+               cursor: pointer;
+            }
             &.active{
-               background-color: red;
-
+               background-color: black;
+               color: white;
+               &:hover{
+                  cursor: default;
+               }
             }
          }
       }
