87112a9948ea931f87ab9edf642c2a7475ae80c3
dashixiong <2788347834@qq.com>
2019-12-14 10:57:28 +0800

    把button相关组件移动到 src/button 下

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-    import Button from '../../../src/button'
+    import Button from '../../../src/button/button'
     import '../../../src/svg'
 
     export default {
--- src/button/button.vue
+++ src/button/button.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script>
-    import Icon from './icon'
+    import Icon from '../icon'
 
     export default {
         components: {
